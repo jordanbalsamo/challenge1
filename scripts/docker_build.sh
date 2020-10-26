e1097b4c4573e03ee893c9501913e866f2a937d1
Jordan Balsamo <jordan.balsamo>
2020-10-26 17:00:56 +0000

    Updating repo -> img

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -4,7 +4,7 @@
 # Assumes that registry is running: docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 if [ $# -eq 0 ]; then
-    echo "Please provide a repo (-r) as a parameter to this script."
+    echo "Please provide a image (-r) as a parameter to this script."
     exit 1
 fi
 
@@ -12,7 +12,7 @@ while getopts r:t: option
 do
     case "${option}"
     in
-        r) REPO=${OPTARG};;
+        r) IMG=${OPTARG};;
     esac
 done
 
@@ -22,17 +22,17 @@ TAG=$(git log -1 --format=%h)
 docker pull python:3.6.1-alpine
 
 # # Build an image from Dockerfile that includes our server code and pass in git commit hash as an image label
-docker image build -t "$REPO:$TAG" --build-arg GIT_COMMIT=$TAG ../../
+docker image build -t "$IMG:$TAG" --build-arg GIT_COMMIT=$TAG ../../
 
 # # Tag the built image locally and prep with registry name for Docker to push to registry
-docker tag "$REPO:$TAG" "localhost:5000/$REPO:$TAG"
+docker tag "$IMG:$TAG" "localhost:5000/$IMG:$TAG"
 
 # # Push locally tagged image to registry
-docker push "localhost:5000/$REPO:$TAG"
+docker push "localhost:5000/$IMG:$TAG"
 
 # # Clean-up local images
-#docker image remove "$REPO:$TAG"
-docker image remove "localhost:5000/$REPO:$TAG"
+#docker image remove "$IMG:$TAG"
+docker image remove "localhost:5000/$IMG:$TAG"
 
 # If we wanted to run the container locally, we can create one from image built via Dockerfile and remove once process ends.
-docker container run -p 5001:5000 --rm "$REPO:$TAG"
+docker container run -p 5001:5000 --rm "$IMG:$TAG"
